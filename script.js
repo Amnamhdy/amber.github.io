b28fd7e6630d2806756495f283282540b22c7296
Amnamhdy <140041865+Amnamhdy@users.noreply.github.com>
2023-07-21 07:44:24 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu =document .querySelector("#menu-icon");
-let navlist =document .querySelector(".navlist");
-
-menu.openclick =() => {
-    menu.classList.toggle("bx-x");
-    navlist.classList.toggle("open");
-}
